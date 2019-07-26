ac34874c7957416fc782295b7fa8a19b17069460
Kenna <kennaalastair@gmail.com>
2019-07-25 17:35:09 -0700

    changed if statement to ternary operatoration for 'update' variable

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,18 +3,11 @@ import './App.css';
 
 import Form from './Components/Form';
 import Member from './Components/Member';
-
 const App = props => {
   const [memberList, setMemberList] = useState(props.members);
   const submit = member => setMemberList([...memberList, member]);
   const update = single => setMemberList([...memberList.map(member => {
-    // single.id === member.id ? single : member;
-    if (single.id === member.id) {
-      return single;
-    }
-    else {
-      return member;
-    }
+    return single.id === member.id ? single : member;
   })]);
 
   const del = single => setMemberList([...memberList.filter(member => member.id !== single.id)]);
